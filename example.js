f06a3af72059fb793464f752f601be64e8be6ddb
KolemanDev <kolemanpa@gmail.com>
2023-09-26 15:59:43 -0500

    .

--- example.js
+++ example.js
@@ -43,7 +43,7 @@ export const applyCustomCode = externalCodeSetup => {
 		console.log("navigator created");
 	});
 
-	externalCodeSetup.navigationApi.replaceScreenComponent("LoginScreen", () => (
+	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
 		<View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
 			<Text>Hello from custom Login Screen</Text>
 			<TextInput placeholder={"login"} />
--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import React from "react";
 import {View, Text} from "react-native";
 export const applyCustomCode = externalCodeSetup => {
     // call custom code api here
-    // externalCodeSetup.configApi.setAppSwitchEnabled(true);
-    externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
-        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Text>This is my signup screen.</Text>
-        </View>
-    ));
+	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+		<View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+			<Text>Hello from custom Login Screen</Text>
+			<TextInput placeholder={"login"} />
+			<TextInput placeholder={"password"} />
+		</View>
+	));
 };
