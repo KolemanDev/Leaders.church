b9e6469c51405a0f09ede71ae6133c435035c2b1
KolemanDev <kolemanpa@gmail.com>
2023-09-27 13:30:23 -0500

    fix

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import {View, Text} from "react-native";
 export const applyCustomCode = externalCodeSetup => {
-	externalCodeSetup.blogApi.showSearch();
+	externalCodeSetup.blogApi.hideSearch();
 
-	externalCodeSetup.profileScreenHooksApi.setAfterProfileHeader(
-	 props => {
-	   console.log(props);
-	   return <Text>showing user id after header, {props.user.id}</Text>
-	 }); 
+
   };
