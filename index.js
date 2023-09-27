5fdde1756205d1837c1560a2dfac10384df5bee2
KolemanDev <kolemanpa@gmail.com>
2023-09-27 13:39:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import {View, Text} from "react-native";
 export const applyCustomCode = externalCodeSetup => {
-
-  };
+    // call custom code api here
+    // externalCodeSetup.configApi.setAppSwitchEnabled(true);
+    externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>This is my signup screen.</Text>
+        </View>
+    ));
+};
