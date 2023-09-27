fa07339fa629245ace303b4173402dc84e40c3ce
KolemanDev <kolemanpa@gmail.com>
2023-09-27 13:22:12 -0500

    testing some functions

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import {View, Text} from "react-native";
 export const applyCustomCode = externalCodeSetup => {
-    // call custom code api here
-	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
-		<View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-			<Text>Hello from custom Login Screen</Text>
-			<TextInput placeholder={"login"} />
-			<TextInput placeholder={"password"} />
-		</View>
-	));
-};
+	externalCodeSetup.blogApi.showSearch();
+
+	externalCodeSetup.profileScreenHooksApi.setAfterProfileHeader(
+	 props => {
+	   console.log(props);
+	   return <Text>showing user id after header, {props.user.id}</Text>
+	 }); 
+  };
