af6bcce0635b4cdcdfafd842495bba5104ea7867
KolemanDev <kolemanpa@gmail.com>
2023-09-28 15:07:52 -0500

    Blockli Connection

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import React from "react";
-import {View, Text} from "react-native";
-export const applyCustomCode = externalCodeSetup => {
-    // call custom code api here
-    // externalCodeSetup.configApi.setAppSwitchEnabled(true);
-    externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
-        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Text>This is my signup screen.</Text>
-        </View>
-    ));
+import { NativeModules } from "react-native";
+const { RNCustomCode } = NativeModules;
+
+import { initialize } from "@blocklienterprise/blockli";
+import config from "@src/build_config.json";
+
+export const applyCustomCode = async (externalCodeSetup) => {
+  const blockli_config = {
+    license: "6ZVSLCJ6C5C5XIQ", // Enter your 15 digit Blockli App Kit license key here. See your account dashboard at https://blockli.dev/dashboard
+    app_id: config.app_id,
+    code: externalCodeSetup,
+    //bcdn_token: "BUNNYCDN-TOKEN-AUTH-KEY",
+    website: "https://leaders.church", // Enter your app domain here with NO trailing slash. 
+    // NOTE: If you have a staging site, please use a different Git branch for your test app and COPY this entire code to the test branch and enter the staging site domain. 
+  };
+
+  await initialize(blockli_config);
 };
