590bdda626ccfe3db616da06e4893ec190d3a1f3
KolemanDev <kolemanpa@gmail.com>
2023-10-16 18:57:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 // index.js
 import React from "react";
 import { NativeModules } from "react-native";
-import { View } from 'react-native';
-import { Text } from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native';
-import lessonThumbnailData from "./lessonThumbnail";
-
-// const { RNCustomCode } = NativeModules;
+const { RNCustomCode } = NativeModules;
 
 import { initialize } from "@blocklienterprise/blockli";
 import config from "@src/build_config.json";
 
-
-
-export const applyCustomCode = externalCodeSetup => {
-  console.log("here?");
-
+export const applyCustomCode = async (externalCodeSetup) => {
   const blockli_config = {
     license: "6ZVSLCJ6C5C5XIQ", // Enter your 15 digit Blockli App Kit license key here. See your account dashboard at https://blockli.dev/dashboard
     app_id: config.app_id,
@@ -24,6 +15,7 @@ export const applyCustomCode = externalCodeSetup => {
     website: "https://leaders.church", // Enter your app domain here with NO trailing slash. 
     // NOTE: If you have a staging site, please use a different Git branch for your test app and COPY this entire code to the test branch and enter the staging site domain. 
   };
+  
 
     // Hide video series description
   externalCodeSetup.courseSingleApi.setIsCourseDescriptionHidden((course) => {
@@ -32,5 +24,5 @@ export const applyCustomCode = externalCodeSetup => {
   // lessonThumbnailData(externalCodeSetup);
 
   await initialize(blockli_config);
-
 };
+
