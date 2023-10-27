7280c4525495842bef83542f65cc6192165e890b
KolemanDev <kolemanpa@gmail.com>
2023-10-27 10:17:56 -0500

    potential fix hide course description

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 import React from "react";
 import { NativeModules } from "react-native";
 const { RNCustomCode } = NativeModules;
@@ -7,19 +6,19 @@ import { initialize } from "@blocklienterprise/blockli";
 import config from "@src/build_config.json";
 
 
-    // Hide video series description
+
+
+export const applyCustomCode = async (externalCodeSetup) => {
     externalCodeSetup.courseSingleApi.setIsCourseDescriptionHidden((course) => {
       return true;
     });
-
-export const applyCustomCode = async (externalCodeSetup) => {
+	
   const blockli_config = {
-    license: "6ZVSLCJ6C5C5XIQ", // Enter your 15 digit Blockli App Kit license key here. See your account dashboard at https://blockli.dev/dashboard
+    license: "6ZVSLCJ6C5C5XIQ",
     app_id: config.app_id,
     code: externalCodeSetup,
     //bcdn_token: "BUNNYCDN-TOKEN-AUTH-KEY",
-    website: "https://leaders.church", // Enter your app domain here with NO trailing slash. 
-    // NOTE: If you have a staging site, please use a different Git branch for your test app and COPY this entire code to the test branch and enter the staging site domain. 
+    website: "https://leaders.church",
   };
     await initialize(blockli_config);
 
