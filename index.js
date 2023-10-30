3809aa577e77ab61b216b99c52e3b08ca51f4b28
KolemanDev <kolemanpa@gmail.com>
2023-10-30 12:20:28 -0500

    Signup screen setup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NativeModules } from "react-native";
 const { RNCustomCode } = NativeModules;
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 import { initialize } from "@blocklienterprise/blockli";
 import config from "@src/build_config.json";
@@ -12,6 +14,12 @@ export const applyCustomCode = async (externalCodeSetup) => {
     externalCodeSetup.courseSingleApi.setIsCourseDescriptionHidden((course) => {
       return true;
     });
+
+    externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+      <WebView
+      source={{ uri: 'https://en225.infusionsoft.app/app/form/create-beta-app-account' }}
+    />
+  ));
 	
   const blockli_config = {
     license: "6ZVSLCJ6C5C5XIQ",
